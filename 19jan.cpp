4c2594e2bcbcfb4113a093057ff10d97a020ce7d
anshkr2006 <anshkr.2006@gmail.com>
2026-01-19 10:18:53 +0530

    19 jan code

---
+++ 19jan.cpp
@@ -0,0 +1,58 @@
+//  using std::
+
+// #include<iostream>
+// int main()
+// {
+//     std::cout<<"hello world";
+//     return 0;
+// }
+
+
+// using namespace
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     cout<<"hello world";
+//     return 0;
+// }
+
+// for taking input of roll number
+// #include<iostream>
+// using namespace std;
+
+// int main()
+// {     int rno;
+//     // printf("\n enter your rno:");
+//     cout<<"\n enter rno:";
+
+//     // scanf("%d",&rno); 
+//     //  replaced by scanf
+//     cin>>rno;                 
+
+//     // printf("you rno. is %d",rno);
+//     cout<<"your rno is :"<<rno;
+
+//     return 0;
+// }
+
+
+// READ 3 SUBJECTS MARKS AND PRINT MARKS ALONG WITH TOTAL
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int m1 ,m2,m3,total;
+//     cout<<"enter m1 :\n";
+//     cout<<"enter m2 :\n";
+//     cout<<"enter m3 :\n";
+
+//     cin>>m1>>m2>>m3; 
+//     cout<<"the marks of three subjects are :\n"<<m1<<endl<<m2<<endl<<m3<<endl;
+
+//     total = m1+m2+m3;
+//     cout<<" total="<<total;
+
+//     return 0;
+// }
+
